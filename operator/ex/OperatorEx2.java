eecc3bcfd9eb15c4237dfe83501178b18e94d2ee
SeongHyun <134840343+dev-hyunnn@users.noreply.github.com>
2024-08-04 14:09:05 +0900

    Add files via upload

---
+++ operator/ex/OperatorEx2.java
@@ -0,0 +1,13 @@
+package operator.ex;
+
+public class OperatorEx2 {
+    public static void main(String[] args) {
+        double var1 = 1.5, var2 = 2.5, var3 = 3.5;
+
+        double sum = var1 + var2 + var3;
+        double average = sum / 3;
+
+        System.out.println(sum);
+        System.out.println(average);
+    }
+}
---
+++ operator/ex/OperatorEx3.java
@@ -0,0 +1,9 @@
+package operator.ex;
+
+public class OperatorEx3 {
+    public static void main(String[] args) {
+        int score = 90;
+        boolean result = 80 <= score && score <= 100;
+        System.out.println(result);
+    }
+}
